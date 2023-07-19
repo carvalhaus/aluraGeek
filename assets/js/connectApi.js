1e105c5df1356979620755f1a81cb38d63d6445e
carvalhaus <ooliveira.joaop@gmail.com>
2023-07-19 06:30:03 -0300

    create js files. Connection api done and show list of products done

--- assets/js/connectApi.js
+++ assets/js/connectApi.js
@@ -1,8 +1,13 @@
-async function listproducts(){
-    
+async function listProducts() {
+  try {
+    const connection = await fetch("http://localhost:3000/products");
+    const connectionJson = await connection.json();
+    return connectionJson;
+  } catch {
+    alert("Erro ao carregar dados!");
+  }
 }
-
 
 export const connectApi = {
-
-}
+  listProducts,
+};
---
+++ assets/js/main.js
@@ -0,0 +1,50 @@
+import { connectApi } from "./connectApi.js";
+
+const starWarsList = document.querySelector("[data-starwars-list]");
+const consoleList = document.querySelector("[data-console-list]");
+const diversosList = document.querySelector("[data-diversos-list]");
+
+function buildProduct(name, price, image) {
+  const products = document.createElement("div");
+  products.className = "product";
+
+  products.innerHTML = `
+      <img
+        src = ${image}
+        alt = ${name}
+        class="product__img"
+      />
+      <p class="description">${name}</p>
+      <p class="price">R$ ${price}</p>
+      <a class="more" href="#">
+        Ver produto
+      </a>
+    `;
+
+  return products;
+}
+
+async function showProducts() {
+  const products = await connectApi.listProducts();
+  products.forEach((element) => {
+    switch (element.categories) {
+      case "starwars":
+        starWarsList.appendChild(
+          buildProduct(element.product_name, element.price, element.url_image)
+        );
+        break;
+      case "console":
+        consoleList.appendChild(
+          buildProduct(element.product_name, element.price, element.url_image)
+        );
+        break;
+      case "diversos":
+        diversosList.appendChild(
+          buildProduct(element.product_name, element.price, element.url_image)
+        );
+        break;
+    }
+  });
+}
+
+showProducts();
